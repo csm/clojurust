b47f90fed740cc16b5e4e1eb199f7907e92a66c4
Casey Marshall <casey@metastatic.org>
2026-04-20 11:37:41 -0700

    Refactor Inst::DefVar pattern matching

--- crates/cljrs-compiler/src/aot.rs
+++ crates/cljrs-compiler/src/aot.rs
@@ -212,7 +212,8 @@ fn collect_defn_arities(
                     // Only consider zero-capture closures (top-level defns).
                     closure_templates.insert(*dst, template.clone());
                 }
-                Inst::DefVar(_, ns, name, val) if let Some(template) = closure_templates.get(val) => {
+                Inst::DefVar(_, ns, name, val)
+                    if let Some(template) = closure_templates.get(val) => {
                     let arities: Vec<ArityInfo> = template
                         .arity_fn_names
                         .iter()
