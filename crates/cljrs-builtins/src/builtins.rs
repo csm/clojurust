539867aa2f46a28820020862ce97e50b79cdfcca
Casey Marshall <casey@metastatic.org>
2026-06-21 10:10:32 -0700

    Fix formatting.

--- crates/cljrs-builtins/src/builtins.rs
+++ crates/cljrs-builtins/src/builtins.rs
@@ -4707,9 +4707,7 @@ fn builtin_partition(args: &[Value]) -> ValueResult<Value> {
                 }
             }
             if padded.len() == n {
-                chunks.push(Value::List(GcPtr::new(PersistentList::from_iter(
-                    padded,
-                ))));
+                chunks.push(Value::List(GcPtr::new(PersistentList::from_iter(padded))));
             }
         }
         start += step;
